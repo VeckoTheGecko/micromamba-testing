bf1db347cfe6addaf3b3939fc6e522fbff46fadd
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-10-21 14:01:12 +0200

    update

--- src/my_package/__init__.py
+++ src/my_package/__init__.py
@@ -1,3 +1,6 @@
-from .utils import load_static_file
+from importlib.resources import files
+
+def load_static_file(name: str) -> str:
+    return files("my_package.static").joinpath(name).read_text(encoding="utf-8")
 
 print(load_static_file("example.txt"))
--- src/my_package/utils.py
+++
@@ -1,5 +0,0 @@
-from importlib.resources import files
-
-def load_static_file(name: str) -> str:
-    """Load static file from the ``virtualship.static`` module by file name."""
-    return files("my_package.static").joinpath(name).read_text(encoding="utf-8")
